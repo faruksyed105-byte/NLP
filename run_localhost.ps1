1757e17b65ea96980365405d5a6a290e8ca974a3
SYED MOHAMMAD FAROOQ <faruksyed105@gmail.com>
2026-08-20 09:11:11 +0530

    Delete run_localhost.ps1

--- run_localhost.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "===================================================================" -ForegroundColor Cyan
-Write-Host "  Starting LexiNLP Platform on http://localhost:8000 ..." -ForegroundColor Green
-Write-Host "===================================================================" -ForegroundColor Cyan
-python run.py --host 127.0.0.1 --port 8000
